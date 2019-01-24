4e5761620d2b08c52f8364a5d364cd891712ddb5
wjkang <977865769@qq.com>
2019-01-24 17:59:25 +0800

    update

--- doc/libs/index.js
+++ doc/libs/index.js
@@ -9,56 +9,57 @@ gitalkConfig = {
   admin: ['wjkang'],
   distractionFreeMode: false
 },
-  window.$docsify = {
-    name: 'NetCore Turorial',
-    repo: '',
-    auto2top: true,
-    loadSidebar: true,
-    subMaxLevel: 5,
-    homepage: 'README.md',
-    search: {
-      noData: {
-        '/': '找不到结果!'
-      },
-      paths: 'auto',
-      placeholder: {
-        '/': '搜索'
-      }
+window.$docsify = {
+  name: 'NetCore Turorial',
+  repo: '',
+  auto2top: true,
+  loadSidebar: true,
+  subMaxLevel: 5,
+  homepage: 'README.md',
+  search: {
+    noData: {
+      '/': '找不到结果!'
     },
-    // plugins: [
-    //   function(hook, vm) {
-    //     hook.beforeEach(function (html) {
-    //       var url = 'https://github.com/wjkang/i-notes/blob/master/' + vm.route.file;
-    //       var editHtml = '[📝 EDIT DOCUMENT](' + url + ')\n';
+    paths: 'auto',
+    placeholder: {
+      '/': '搜索'
+    }
+  },
+  // plugins: [
+  //   function(hook, vm) {
+  //     hook.beforeEach(function (html) {
+  //       var url = 'https://github.com/wjkang/i-notes/blob/master/' + vm.route.file;
+  //       var editHtml = '[📝 EDIT DOCUMENT](' + url + ')\n';
 
-    //       return editHtml + html;
-    //     })
+  //       return editHtml + html;
+  //     })
 
-    //     hook.doneEach(function(){
-    //       var label, domObj, main, divEle, gitalk;
-    //       label = vm.route.path.split('/').join('');
-    //       domObj = Docsify.dom;
-    //       main = domObj.getNode("#main");
+  //     hook.doneEach(function(){
+  //       var label, domObj, main, divEle, gitalk;
+  //       label = vm.route.path.split('/').join('');
+  //       domObj = Docsify.dom;
+  //       main = domObj.getNode("#main");
 
-    //       /**
-    //        * render gittalk
-    //        */
-    //       Array.apply(null,document.querySelectorAll("div.gitalk-container")).forEach(function(ele){ele.remove()});
-    //       divEle = domObj.create("div");
-    //       divEle.id = "gitalk-container-" + label;
-    //       divEle.className = "gitalk-container";
-    //       divEle.style = "width: " + main.clientWidth + "px; margin: 0 auto 20px;";
-    //       domObj.appendTo(domObj.find(".content"), divEle);
-    //       gitalk = new Gitalk(Object.assign(gitalkConfig, {id: !label ? "home" : label}))
-    //       gitalk.render('gitalk-container-' + label)
-    //     })
-    //   }
-    // ]
-  }
-
+  //       /**
+  //        * render gittalk
+  //        */
+  //       Array.apply(null,document.querySelectorAll("div.gitalk-container")).forEach(function(ele){ele.remove()});
+  //       divEle = domObj.create("div");
+  //       divEle.id = "gitalk-container-" + label;
+  //       divEle.className = "gitalk-container";
+  //       divEle.style = "width: " + main.clientWidth + "px; margin: 0 auto 20px;";
+  //       domObj.appendTo(domObj.find(".content"), divEle);
+  //       gitalk = new Gitalk(Object.assign(gitalkConfig, {id: !label ? "home" : label}))
+  //       gitalk.render('gitalk-container-' + label)
+  //     })
+  //   }
+  // ]
+}
+var blackCat='./libs/live2d-widget/live2d-widget-model-hijiki/assets/hijiki.model.json';
+var whiteCat:'./libs/live2d-widget/live2d-widget-model-tororo/assets/tororo.model.json',
 L2Dwidget.init({
   model: {
-    jsonPath: './libs/live2d-widget/live2d-widget-model-hijiki/assets/hijiki.model.json',
+    jsonPath: Math.random()>.5?blackCat:whiteCat,
   },
   display: {
     width: 50,
